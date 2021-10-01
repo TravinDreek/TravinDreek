accac54991b936a261447d1817b8142075836be9
TravinDreek <1244050218@qq.com>
2021-10-01 21:54:42 +0800

    Preparing for parallax

--- js/main.js
+++ js/main.js
@@ -1,32 +1,39 @@
-function NewTab(url) {
+function newTab(url) {
 	window.open(url, "_blank");
 }
 
 window.onload = function() {
 	const title = document.getElementById('title');
 	const about = document.getElementById('about');
-	let targetX = window.innerWidth / 2;
-	let targetY = window.innerHeight / 2;
-	let titleX = targetX;
-	let titleY = targetY;
+	let mouseX = window.innerWidth / 2;
+	let mouseY = window.innerHeight / 2;
+
+	const elemPara = [
+		{
+			element: title,
+			x: 0,
+			y: 0,
+			z: 10
+		}
+	];
 
 	function update() {
-		titleX = titleX + (targetX - titleX) * .1;
-		titleY = titleY + (targetY - titleY) * .1;
-		title.style.left = titleX + 'px';
-		title.style.top = titleY + 'px';
+		for(i in elemPara) {
+			elemPara[i].element.style.left = elemPara[i].element.offsetLeft - (elemPara[i].element.offsetLeft - window.innerWidth / 2 + (mouseX - window.innerWidth / 2) * 0.01 * elemPara[i].z - elemPara[i].x) * .1 + 'px';
+			elemPara[i].element.style.top = elemPara[i].element.offsetTop - (elemPara[i].element.offsetTop - window.innerHeight / 2 + (mouseY - window.innerHeight / 2) * 0.01 * elemPara[i].z - elemPara[i].y) * .1 + 'px';
+		}
 		requestAnimationFrame(update);
 	}
 	update();
-	
+
 	window.onmousemove = function(e) {
-		targetX = window.innerWidth / 2 + (window.innerWidth / 2 - e.clientX) * .05;
-		targetY = window.innerHeight / 2 + (window.innerHeight / 2 - e.clientY) * .05;
+		mouseX = e.clientX;
+		mouseY = e.clientY;
 	};
 
 	window.onresize = function(e) {
-		targetX = window.innerWidth / 2;
-		targetY = window.innerHeight / 2;
+		mouseX = window.innerWidth / 2;
+		mouseY = window.innerHeight / 2;
 	}
 
 	title.onclick = function(e) {
